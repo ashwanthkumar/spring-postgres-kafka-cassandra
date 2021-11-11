6331a4a6cfdf62563566e293746762d775acf8d9
Ashwanth Kumar <ashwanthkumar@googlemail.com>
2021-11-11 10:58:41 +0530

    Initial commit of Spring Initialzr project

---
+++ src/main/java/in/ashwanthkumar/springpostgreskafkacassandra/SpringPostgresKafkaCassandraApplication.java
@@ -0,0 +1,13 @@
+package in.ashwanthkumar.springpostgreskafkacassandra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPostgresKafkaCassandraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPostgresKafkaCassandraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/ashwanthkumar/springpostgreskafkacassandra/SpringPostgresKafkaCassandraApplicationTests.java
@@ -0,0 +1,13 @@
+package in.ashwanthkumar.springpostgreskafkacassandra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPostgresKafkaCassandraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
